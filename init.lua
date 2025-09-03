9211174bb173a8f6e1247b6661e6965d3e2c8813
Andrew Furey <andrewfurey2003@gmail.com>
2025-09-03 12:46:22 +0100

    trying to speed up neovim

--- init.lua
+++ init.lua
@@ -791,5 +791,18 @@ require('lazy').setup({
   },
 })
 
+require('nvim-treesitter.configs').setup {
+  highlight = {
+    enable = true,
+    disable = function(lang, buf)
+      local max_filesize = 100 * 1024 -- 100 KB
+      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+      if ok and stats and stats.size > max_filesize then
+        return true
+      end
+    end,
+  },
+}
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
